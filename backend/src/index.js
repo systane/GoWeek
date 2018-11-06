8963ac0ab80c292f91f38547c13a4f4c74afeed8
Luis Fernando <lsilva49@csc.com>
2018-11-06 17:33:09 -0200

    Adição do LikeController e também adição do socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,31 @@
 const express = require('express'); //importa a dependencia do node_modules (express)
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express(); //instancia a aplicação
+
+const server = require('http').Server(app); //Extrai o servidor http criado com o express
+const io = require('socket.io')(server); //habilita o servidor para escutar o protocolo WS(WebSocket), além do Http comum
+
 
 mongoose.connect('mongodb://root:root123@ds155203.mlab.com:55203/goweek-systane', {
     useNewUrlParser: true
 });
 
+
+//Cria um middleware
+app.use((req, res, next) =>{
+    req.io = io; //Cria uma nova variavel em cada requisição
+
+    return next(); //Continua com a próxima requisição
+});
+
+app.use(cors());//habilita o cors
+app.use(express.json());//Indica que as requisições serão em json
 app.use(require('./routes'));
 
 //Aloca a aplicação (app) na porta 3000 e executa uma função de callback quando o servidor iniciar
-app.listen(3000, ()=>{
+server.listen(3000, ()=>{
     console.log('Server started on port 3000 :)');
 });
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,12 @@ const express = require('express');
 const routes = express.Router();//Módulo de rotas do express
 
 const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
 
-routes.get('tweets', TweetController.index);
-routes.post('tweets', TweetController.store);
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
+
+
+routes.post('/likes/:id', LikeController.store);
 
 module.exports = routes;
