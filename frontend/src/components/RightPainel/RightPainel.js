3fd9ab5537df263016f06950956ac0f0a44551f5
Luis Fernando <lf.silva@outlook.com>
2018-11-13 11:40:13 -0200

    Avanços na estilização do frontend da home

--- frontend/src/components/RightPainel/RightPainel.js
+++ frontend/src/components/RightPainel/RightPainel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import twitterLogo from '../../twitter.svg';
 import { Link } from 'react-router-dom';
-import RightPainelWrapper from './RightPainelStyle';
+import {RightPainelWrapper, LoginLinkWrapper} from './RightPainelStyle';
 import Grid from '@material-ui/core/Grid';
 
 export default class RightPainel extends Component {
@@ -16,13 +16,19 @@ export default class RightPainel extends Component {
                 >
                     <Grid item md={6}>
                         <img src={twitterLogo} alt="GoTwitter" />
-                        <Link to="/Login">Entrar</Link>
+                    </Grid>
+                    <Grid item md={6}>
+                        <LoginLinkWrapper>
+                            <Link to="/Login" style={{textDecoration: "none"}}>Entrar</Link>
+                        </LoginLinkWrapper>
                     </Grid>
                 </Grid>
                 <h1>Veja o que está acontecendo no mundo agora</h1>
                 <h2>Participe hoje do Twitter.</h2>
-                {/* <Link>Inscreva-se</Link> */} 
-                <Link to="/Login">Entrar</Link>
+                {/* <Link>Inscreva-se</Link> */}
+                <LoginLinkWrapper>
+                    <Link to="/Login">Entrar</Link>
+                </LoginLinkWrapper>
             </RightPainelWrapper>
         );
     }
--- frontend/src/components/RightPainel/RightPainelStyle.js
+++ frontend/src/components/RightPainel/RightPainelStyle.js
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
+const LoginLinkWrapper = styled.a`
+
+    text-decoration: none !important;
+    border-radius: 100px;
+    box-shadow: none;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 20px;
+    padding: 6px 16px;
+    position: relative;
+    text-align: center;
+    white-space: nowrap;
+
+    background-color: #fff;
+    border: 1px solid #1da1f2;
+    color: #1da1f2;
+
+    &:hover{
+        background-color: #E2FFFF;
+    }
+
+    &:hover, &:visited, &:link, &:active{
+        text-decoration: none !important;
+    }
+`;
+
 const RightPainelWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -12,15 +39,23 @@ const RightPainelWrapper = styled.div`
     img{
         width:40px;
         height:40px;
+        display: flex;
+        justify-content: left;
     }
 
     h1{
         word-wrap: break-word;
+        text-align:left;
+        font-size: 30px;
+        margin-right: 100px;
+        margin-bottom: 40px;
     }
 
     h2{
-        font: 20px bold;
+        font-size: 20px;
+        text-align: left;
+        margin-bottom: 10px;
     }
 `;
 
-export default RightPainelWrapper;
+export {RightPainelWrapper, LoginLinkWrapper};
